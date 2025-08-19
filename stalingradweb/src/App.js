72d3c3ccff2f3b5a73d844af0352414e12dc9f87
LUCAS JOSUÉ MAIA <lucas_j_maia@estudante.sesisenai.org.br>
2025-08-19 08:45:29 -0300

    recursos e coisas que usaremos futuramente no site

---
+++ stalingradweb/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (  
+    <div>
+      <div className='banner'>
+        <h1>Stalingrad: ponto de virada no Leste</h1>
+        <h2>Stalingrado e pesquisando a experiência de guerra</h2>
+      </div>
+      <div className='content'>
+        <h2>Sobre</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo dolor ut orci fringilla lacinia. Proin imperdiet auctor ante ut eleifend. Etiam convallis neque quis pretium pretium. Morbi cursus risus efficitur, mattis felis ac, mollis ligula. Nulla at luctus velit. Morbi vulputate sem eu enim viverra pretium. Mauris mattis ligula vitae mauris sollicitudin, in accumsan magna auctor. </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
